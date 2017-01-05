e813406189ce3eb5cabf8bc89bca3e244d583b76
Brian Lykes <blykes@gmail.com>
2017-01-05 01:35:32 -0500

    Add files via upload

---
+++ 10sec.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+  for(int a=0;a<10000;a++){
+    for(int b=0;b<1000;b++){
+      for(int c=0;c<600;c++){
+      }
+    }
+  }
+	  
+
+return 0;
+
+}
